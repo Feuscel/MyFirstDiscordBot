4ef3889ac382bf85457279d79d66c63d18c8f7ef
Feuscel <feuscel@gmail.com>
2022-04-22 15:14:06 +0200

    just for eslint

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
-const { token } = require('./config.json');
-const configuration = require('./configFile.js');
-
-const Discord = require('discord.js');
-require('discord-reply');
-const client = new Discord.Client();
-configuration.dotenv.config();
+import { token } from './config.json';
+import { dotenv, configCommand, prefix } from './configFile.js';
+import { Client, Collection } from 'discord.js';
+import 'discord-reply';
+const client = new Client();
+dotenv.config();
 
 client.once('ready', () => {
-	client.commands = new Discord.Collection();
-	configuration.configCommand(client.commands);
+	client.commands = new Collection();
+	configCommand(client.commands);
 	console.log('Ready!');
 });
 
-client.login(token).then(r => console.log('I\'m connected'));
+client.login(token).then(() => console.log('I\'m connected'));
 
 client.on('message', message => { if (isPrefix(message)) executeCommand(message); });
 
@@ -30,13 +29,13 @@ function executeCommand(message) {
 	}
 }
 
-function isPrefix(message) { return !(!message.content.startsWith(configuration.prefix) || message.author.bot); }
+function isPrefix(message) { return !(!message.content.startsWith(prefix) || message.author.bot); }
 
 function isCommand(commandName) { return client.commands.has(commandName); }
 
 function haveArg(command, args) { return command.args && !args.length; }
 
-function getArgs(message) { return message.content.slice(configuration.prefix.length).trim().split(/ +/); }
+function getArgs(message) { return message.content.slice(prefix.length).trim().split(/ +/); }
 
 function getCommand(args, message) {
 	const commandName = args.shift().toLowerCase();
